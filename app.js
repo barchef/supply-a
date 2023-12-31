f3cfea3af3585f6acc45ec6516bcb8e2b29e89a0
TheBarChef <marlonjwilliams@gmail.com>
2023-12-31 12:46:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -18,9 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // return hardcoded PLS supply for now
 // at a future date, need to traverse the chain
